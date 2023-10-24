44f949763df20c31fb44393d497abdae5ab04628
Samir Hassen <samirhassen14@gmail.com>
2023-10-23 21:37:15 -0500

    Managing large projects launch tutorial

--- src/launch_tutorial/setup.py
+++ src/launch_tutorial/setup.py
@@ -9,6 +9,10 @@ setup(
     data_files=[
         # ... Other data files
         # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Include config files
+        (os.path.join('share', package_name, 'config'),
+         glob(os.path.join('config', '*.yaml'))),
     ]
 )
